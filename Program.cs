2f9c29cadaa1e40f63835bceb184c97ea5428c4b
Marco-Merini <marcoleonemerini@gmail.com>
2026-03-26 20:10:46 -0300

    Primeiro commit com a tela principal

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using NutriFlow.Services;
+using MudBlazor.Services;
+using NutriFlow.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+// Register application services
+// Typed HttpClient for ApiService will be provided by IHttpClientFactory
+builder.Services.AddHttpClient<ApiService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<NavigationService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
